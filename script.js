8a5dab4fd58d5cb738ba00c7f09a0cd0f5f6979b
Said ARSLAN <lost.myreligion@hotmail.com>
2023-05-08 03:41:41 +0300

    In Javascript Added display function, adjusted css styles

--- script.js
+++ script.js
@@ -5,12 +5,20 @@ const numbers = [];
 
 function createNumber() {
 
-    for (let index = 0; index <= 20; index++) {
+    for (let index = 0; index <= 11; index++) {
         const number = Math.floor(Math.random() * 10);
         numbers.push(number);
     }
     
 }
+createNumber();
 function displayGame() {
-    console.log("test");
+    for (let i of numbers) {
+        const card = ` <div class="game-card">
+                    <h3>${i}</h3>
+                    </div>
+                    `
+        game.insertAdjacentHTML("beforeend",card);
+    }
 }
+displayGame();
